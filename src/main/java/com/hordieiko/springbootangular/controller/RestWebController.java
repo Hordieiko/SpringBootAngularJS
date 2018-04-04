31dc003d3fc9e61c2bb3c2a40e79a97fb6e077bd
Hordieiko Vasiliy <vhordieiko@determine.com>
2018-04-04 20:15:57 +0300

    Init Project Structure

---
+++ src/main/java/com/hordieiko/springbootangular/controller/RestWebController.java
@@ -0,0 +1,7 @@
+package com.hordieiko.springbootangular.controller;
+
+/**
+ * Created by vhordieiko on 4/4/2018.
+ */
+public class RestWebController {
+}
---
+++ src/main/resources/static/js/controller.js
@@ -0,0 +1,3 @@
+/**
+ * Created by vhordieiko on 4/4/2018.
+ */
